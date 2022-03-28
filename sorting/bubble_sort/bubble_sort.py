42ac1337d16e06342bb22f1231d29a7488ea4893
mmizak <01169211@pw.edu.pl>
2022-03-28 17:17:25 +0200

    slowed bubble sort down

--- sorting/bubble_sort/bubble_sort.py
+++ sorting/bubble_sort/bubble_sort.py
@@ -1,11 +1,8 @@
 def bubble_sort(unsorted_list):
-    swap = True
     partition = 1
-    while swap:
-        swap = False
-        for i in range(len(unsorted_list) - partition):
+    while partition < len(unsorted_list):
+        for i in range(len(unsorted_list) - 1):
             if unsorted_list[i] > unsorted_list[i + 1]:
                 unsorted_list[i], unsorted_list[i + 1] = unsorted_list[i + 1], unsorted_list[i]
-                swap = True
         partition += 1
     return unsorted_list

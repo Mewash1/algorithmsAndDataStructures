51b3afbbeeee4666afc273de25700bd35857bc37
Michał Jakomulski <bigz1001@gmail.com>
2022-04-09 14:25:18 +0200

    draft of insert funciotn

--- trees/binary_tree_dir/binary_tree.py
+++ trees/binary_tree_dir/binary_tree.py
@@ -5,21 +5,74 @@ class Node:
         self.left = None
 
 
-def create_BT(node: Node, data):
+def create_BT(node: Node, data, used_data=list()):
     if node is None:
         node = Node(data)
 
     if node.key > data:
-        node.left = create_BT(node.left, data)
-    elif node.key < data:
-        node.right = create_BT(node.right, data)
+        used_data.pop()
+        node.left, used_data = create_BT(node.left, data, used_data)
+    elif node.key < data or (node.key == data and data in used_data):
+        used_data.pop()
+        node.right, used_data = create_BT(node.right, data, used_data)
 
-    return node
+    used_data.append(data)
+
+    return node, used_data
 
 
 def create_BT_loop(data):
     root = None
+    used_data = None
     for value in data:
-        root = create_BT(root, value)
+        root, used_data = create_BT(root, value)
 
     return root
+
+
+def search_BT(root: Node, key):
+    if root is None or root.key == key:
+        return root
+
+    if key < root.key:
+        root = search_BT(root.left, key)
+    else:
+        root = search_BT(root.right, key)
+    return root
+
+
+def insert_nood_BT(node: Node, data):
+    if node is None:
+        node = Node(data)
+
+    if node.key > data:
+        node.left = insert_nood_BT(node.left, data)
+    elif node.key < data:
+        node.right = insert_nood_BT(node.right, data)
+
+    return node
+
+# def print_tree(root: Node):
+#     if root is not None:
+#         print(root.key)
+#         if root.left is not None:
+#             print(root.left.key, end=' ')
+#         else:
+#             print('-', end=' ')
+#         if root.right is not None:
+#             print(root.right.key)
+#         else:
+#             print('-')
+#         print_tree(root.left)
+#         print_tree(root.right)
+
+
+# def tree_deep(root: Node, deep):
+#     if root.left is not None:
+#         deep_l += 1
+#         deep_l, deep_r = tree_deep(root.left, deep)
+#     elif root.right is not None:
+#         deep_r += 1
+#         deep_l, deep_r = tree_deep(root.right, deep)
+
+#     return deep_l, deep_r
--- trees/binary_tree_dir/test_binary_tree.py
+++ trees/binary_tree_dir/test_binary_tree.py
@@ -1,7 +1,23 @@
-from binary_tree import (
-    Node, create_BT, create_BT_loop
+from binary_tree_dir.binary_tree import (
+    create_BT_loop, search_BT, insert_nood_BT
 )
 
+
 def test_ehh():
-    data = [5, 4, 2, 6, 8]
+    data = [5, 4, 2, 6, 8, 8]
     root = create_BT_loop(data)
+    insert_nood_BT(root, 7)
+    pass
+
+
+# data = [5, 4, 2, 6, 8]
+# root = create_BT_loop(data)
+# inorder(root)
+
+
+def test_search_BT():
+    data = [5, 4, 66, 3, 34, 5]
+    root = create_BT_loop(data)
+    key = 34
+    key_root = search_BT(root, key)
+    assert key_root.key == 34

490bb5b97e20e35dced775dfb394ea63323571f4
Michał Jakomulski <bigz1001@gmail.com>
2022-05-20 11:27:54 +0200

    working KR_search with tests

--- patter_search/KR_search.py
+++ patter_search/KR_search.py
@@ -1,45 +1,20 @@
 def KR_search(pattern, text):
-    pattern_index_in_text = 0
-    patterns_in_text = []
-    if pattern == '' and text == '':
-        return patterns_in_text
-    while pattern_index_in_text <= (len(text)) - len(pattern):
-        prefix_index = pattern_index_in_text
-        prefix_len = 0
-        for i in range(len(pattern)):
-                pattern_letter = pattern[i]
-                text_letter = text[prefix_index]
-                if pattern_letter != text_letter:
+    length = len(text) - len(pattern)
+    # stores indexes where pattern is in text
+    pattern_in_text = []
+    if (length < 0 or
+       (len(pattern) == 0 and len(text) == 0)):
+        return pattern_in_text
+    pattern_hash = hash(pattern)
+    for i in range(length+1):
+        try_pattern = text[i:len(pattern)+i]
+        try_pattern_hash = hash(try_pattern)
+        if pattern_hash == try_pattern_hash:
+            for j in range(len(pattern)):
+                pattern_letter = pattern[j]
+                try_pattern_letter = try_pattern[j]
+                if pattern_letter != try_pattern_letter:
                     break
-                else:
-                    prefix_len += 1
-                    prefix_index += 1
-        else:
-            patterns_in_text.append(pattern_index_in_text)
-            pattern_index_in_text += 1
-            continue
-        prefix = text[pattern_index_in_text:prefix_index]
-        pre_su_fix = prefix_search(prefix)
-        pattern_index_in_text += prefix_len - pre_su_fix
-
-    return patterns_in_text
-
-
-def prefix_search(pattern) -> int:
-    if len(pattern) == 0:
-        return -1
-    id1 = 0
-    id2 = len(pattern) - 1
-    while id2 != 0:
-        id1 += 1
-        prefix = pattern[:id2]
-        sufix = pattern[id1:]
-        if prefix == sufix:
-            return len(prefix)
-        id2 -= 1
-    return 0
-
-
-
-
-
+            else:
+                pattern_in_text.append(i)
+    return pattern_in_text
--- patter_search/tests/test_KR_search.py
+++ patter_search/tests/test_KR_search.py
@@ -1,35 +1,4 @@
-from KR_search import (
-    prefix_search,
-    KR_search
-)
-
-
-def test_prefix_search():
-    prefix = 'abccab'
-    assert prefix_search(prefix) == 2
-    prefix = 'abcefefabce'
-    assert prefix_search(prefix) == 4
-
-
-def test_KR_search_easy():
-    text = 'tralalatralatytrala'
-    pattern = 'alal'
-    mathes = KR_search(pattern, text)
-    assert mathes == [2]
-
-
-def test_KR_search_easy2():
-    text = 'abababacababacacab'
-    pattern = 'abababab'
-    mathes = KR_search(pattern, text)
-    assert mathes == []
-
-
-def test_KR_search_easy3():
-    text = 'abacabacababacacab'
-    pattern = 'abacabab'
-    mathes = KR_search(pattern, text)
-    assert mathes == [4]
+from KR_search import KR_search
 
 
 def test_empty_strings():
